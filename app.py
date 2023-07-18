24bd7dbd43554119bfee0bbc8844e7f68936bfc9
Neha Darekar <138971615+NehaDarekar777@users.noreply.github.com>
2023-07-18 12:18:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -54,5 +54,5 @@ def predict():
     return render_template("index.html",prediction =loan_result)
 
 if __name__== "__main__":
-    app.run(host= "0.0.0.0",port=8080, debug=True) ##### AWS Deployment host = 0.0.0.0 port= 8080 debug= False
+    app.run(host= "0.0.0.0",port=8090, debug=True) ##### AWS Deployment host = 0.0.0.0 port= 8080 debug= False
 
